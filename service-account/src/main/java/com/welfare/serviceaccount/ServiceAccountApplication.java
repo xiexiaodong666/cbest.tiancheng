27f59d1415c55b7d27b9257fe6e090da419ee02b
liyx <liyx@gillion.com.cn>
2021-01-06 14:40:49 +0800

    删除不必要得得启动项ç

--- service-account/src/main/java/com/welfare/serviceaccount/ServiceAccountApplication.java
+++ service-account/src/main/java/com/welfare/serviceaccount/ServiceAccountApplication.java
@@ -8,18 +8,7 @@ import org.springframework.core.env.SimpleCommandLinePropertySource;
 @SpringBootApplication
 public class ServiceAccountApplication {
 
-    private static  final String PROFILES = "spring.profiles.active";
-    private static String env = null;
-
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(ServiceAccountApplication.class);
-        application.setBannerMode(Banner.Mode.OFF);
-        SimpleCommandLinePropertySource commandLineArgs = new SimpleCommandLinePropertySource(args);
-        env = commandLineArgs.getProperty(PROFILES);
-        if (env == null) {
-            env = "dev";
-        }
-        application.setAdditionalProfiles(env);
-        application.run(args);
+        SpringApplication.run(ServiceAccountApplication.class);
     }
 }

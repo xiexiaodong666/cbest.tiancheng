c327addf7d2b165b84a4a77f9909fd1019fc624c
duanhy <dhongyuan@foxmail.com>
2021-01-07 15:44:42 +0800

    加日志

--- service-merchant/src/main/java/com/welfare/servicemerchant/controller/AccountDepositController.java
+++ service-merchant/src/main/java/com/welfare/servicemerchant/controller/AccountDepositController.java
@@ -1,6 +1,7 @@
 package com.welfare.servicemerchant.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.welfare.servicemerchant.dto.AccountDepositRequest;
 import com.welfare.servicemerchant.dto.AccountInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -9,13 +10,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.dreamlu.mica.common.support.IController;
 import net.dreamlu.mica.core.result.R;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
- * 员工账号余额管理
+ * 员工账号存款管理
  * @author duanhy
  * @version 1.0.0
  * @description
@@ -25,8 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/account-balance")
-@Api(tags = "员工账号余额管理")
-public class AccountBalanceController implements IController {
+@Api(tags = "员工账号存款管理")
+public class AccountDepositController implements IController {
 
   @GetMapping("/apply/page")
   @ApiOperation("分页账号额度申请列表")
@@ -75,18 +75,15 @@ public class AccountBalanceController implements IController {
     return null;
   }
 
-  @GetMapping("/apply/save")
+  @PostMapping("/apply/save")
   @ApiOperation("新增额度申请")
-  public R<Page<AccountInfo>> save(@RequestParam @ApiParam("当前页") Integer currentPage,
-                                       @RequestParam @ApiParam("单页大小") Integer pageSize,
-                                       @RequestParam(required = true) @ApiParam("门店编码") String storeCode,
-                                       @RequestParam(required = true) @ApiParam("查询开始的id > 0)") Long startId){
+  public R<Long> save(@RequestBody List<AccountDepositRequest> request){
     return null;
   }
 
   @GetMapping("/apply/batch-save")
   @ApiOperation("批量新增额度申请")
-  public R<Page<AccountInfo>> batchSave(@RequestParam @ApiParam("当前页") Integer currentPage,
+  public R<Long> batchSave(@RequestParam @ApiParam("当前页") Integer currentPage,
                                        @RequestParam @ApiParam("单页大小") Integer pageSize,
                                        @RequestParam(required = true) @ApiParam("门店编码") String storeCode,
                                        @RequestParam(required = true) @ApiParam("查询开始的id > 0)") Long startId){

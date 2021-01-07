c13ef3ff0729c7521749a491df4800e7bc10b9cb
liyx <liyx@gillion.com.cn>
2021-01-07 13:52:25 +0800

    充值

--- service-account/src/main/java/com/welfare/serviceaccount/controller/DepositController.java
+++ service-account/src/main/java/com/welfare/serviceaccount/controller/DepositController.java
@@ -1,11 +1,16 @@
 package com.welfare.serviceaccount.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.welfare.serviceaccount.domain.Deposit;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.dreamlu.mica.common.support.IController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import net.dreamlu.mica.core.result.R;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Description:
@@ -21,4 +26,21 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "充值相关接口")
 public class DepositController implements IController {
 
+    @PostMapping
+    @ApiOperation("发起充值")
+    public R<Deposit> newDeposit(@RequestBody Deposit deposit){
+        return null;
+    }
+
+    @PostMapping("/batch")
+    @ApiOperation("批量充值")
+    public R<List<Deposit>> newDepositBatch(@RequestBody List<Deposit> deposits){
+        return null;
+    }
+
+    @GetMapping("/singleQuery")
+    @ApiOperation("根据requestId查询充值结果")
+    public R<Deposit> getByRequestId(@PathVariable String requestId){
+        return null;
+    }
 }
--- service-account/src/main/java/com/welfare/serviceaccount/domain/Deposit.java
+++ service-account/src/main/java/com/welfare/serviceaccount/domain/Deposit.java
@@ -16,10 +16,16 @@ import java.math.BigDecimal;
 @Data
 @ApiModel("充值请求")
 public class Deposit {
+    @ApiModelProperty("请求id")
+    private String requestId;
     @ApiModelProperty("账户")
     private String accountCode;
     @ApiModelProperty("充值卡号")
     private String cardNo;
     @ApiModelProperty("充值金额")
     private BigDecimal amount;
+    @ApiModelProperty("充值来源")
+    private String depositSource;
+    @ApiModelProperty(value = "充值状态",notes = "1:新增, 2:处理中, 3:处理成功 -1:处理失败")
+    private Integer depositStatus;
 }

7e5642aad9e96e75b60bf0825f1c7a2a1dd6dd15
殷豪 <84664373@qq.com>
2021-01-07 15:47:34 +0800

    新增商户列表接口定义； 新增商户部门列表接口定义

--- service-merchant/src/main/java/com/welfare/servicemerchant/controller/DepartmentController.java
+++ service-merchant/src/main/java/com/welfare/servicemerchant/controller/DepartmentController.java
@@ -1,7 +1,7 @@
 package com.welfare.servicemerchant.controller;
 
 import com.welfare.service.MerchantService;
-import com.welfare.servicemerchant.dto.MerchantInfo;
+import com.welfare.servicemerchant.dto.DepartmentInfo;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
@@ -9,30 +9,27 @@ import lombok.extern.slf4j.Slf4j;
 import net.dreamlu.mica.common.support.IController;
 import net.dreamlu.mica.core.result.R;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 /**
- * 商户信息服务控制器
- *
- * @author Yuxiang Li
- * @since 2021-01-06 13:49:25
- * @description 由 Mybatisplus Code Generator 创建
+ * 商户部门服务控制器
+ * Created by hao.yin on 2021/1/7.
  */
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/merchant")
-public class MerchantController implements IController {
+@RequestMapping("/department")
+public class DepartmentController implements IController {
     private final MerchantService merchantService;
     @GetMapping("/list")
-    @ApiOperation("查询商户列表（不分页）")
-    public R<List<MerchantInfo>> list(@RequestParam @ApiParam("商户类型") String merType,
-                                      @RequestParam @ApiParam("身份属性") String merIdentity,
-                                      @RequestParam @ApiParam("合作方式") String merCooperationMode){
+    @ApiOperation("根据商户代码查询商户部门列表（不分页）")
+    public R<List<DepartmentInfo>> list(@RequestParam(required = true) @ApiParam("商户代码") String merCode,
+                                        @RequestParam @ApiParam("部门父级") String departmentParent,
+                                        @RequestParam @ApiParam("部门层级") String departmentLevel){
         return null;
     }
 }

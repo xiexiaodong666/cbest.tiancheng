9ccf978ac2b6318d4e149c98545abb2d359ab00b
liyx <liyx@gillion.com.cn>
2021-01-07 19:17:21 +0800

    deploy.sh

--- service-merchant/deploy.sh
+++ service-merchant/deploy.sh
@@ -4,7 +4,7 @@ scp ./target/e-welfare-merchant.jar root@172.30.37.188:/home/finance/App/e-welfa
 echo "sleeping 3 secs"
 sleep 3
   ssh root@172.30.37.188 "
-chown  -R  finance:finance /home/finance/App/e-welfare-account.e-cbest.lotest/release;
+chown  -R  finance:finance /home/finance/App/e-welfare-merchant.e-cbest.lotest/release;
 sh /home/finance/app-shell/run.sh stop e-welfare-merchant;
 sleep 5;
 sh /home/finance/app-shell/run.sh start e-welfare-merchant;

623d5a9778396a2ac88e1cb1be639238ea5edefb
dengqiang <17783863034@163.com>
2021-01-08 15:06:29 +0800

    结算模块添加presto数据源

---
+++ common/src/main/java/com/welfare/common/enums/DataBaseTypeEnum.java
@@ -0,0 +1,45 @@
+package com.welfare.common.enums;
+
+/**
+ * @Author: qiang.deng01
+ * @Date: 2021/01/08 9:49
+ * 多数据源枚举
+ */
+public enum DataBaseTypeEnum {
+    MYSQL_DB("mysql","mysql数据源"),
+    PRESTO_DB("presto","presto数据源");
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    private String code;
+
+    private String desc;
+
+    DataBaseTypeEnum(String code, String desc){
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public static String getDescByCode(String code){
+        for (DataBaseTypeEnum baseTypeEnum : DataBaseTypeEnum.values()){
+            if(baseTypeEnum.code.equals(code)){
+                return baseTypeEnum.desc;
+            }
+        }
+        return null;
+    }
+}
--- service-settlement/src/main/java/com/welfare/servicesettlement/task/SettlementWorkXxlJobTaskBean.java
+++ service-settlement/src/main/java/com/welfare/servicesettlement/task/SettlementWorkXxlJobTaskBean.java
@@ -1,6 +1,7 @@
 package com.welfare.servicesettlement.task;
 
 
+import com.alibaba.fastjson.JSON;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.IJobHandler;
 import com.xxl.job.core.util.ShardingUtil;
@@ -20,7 +21,7 @@ public abstract class SettlementWorkXxlJobTaskBean<T> extends IJobHandler {
 	/**
 	 * 分片数量
 	 */
-	protected Integer taskItemNum;
+	protected Integer taskItemNum = 1;
 	/**
 	 * 单次处理列表量
 	 */
@@ -28,25 +29,31 @@ public abstract class SettlementWorkXxlJobTaskBean<T> extends IJobHandler {
 	/**
 	 * 命中的分片序号
 	 */
-	protected Integer task_item;
+	protected Integer taskItem = 0;
 
 	@Override
 	public ReturnT<String> execute(String pramas){
 		log.info("============"+this.getClass().getName()+"任务执行开始===================");
 		ShardingUtil.ShardingVO shardingVo = ShardingUtil.getShardingVo();
 		taskItemNum = shardingVo.getTotal();
-		task_item = shardingVo.getIndex();
+		taskItem = shardingVo.getIndex();
 
 		List<T> dealList;
 
-		boolean result;
+		boolean result = false;
 
 		do {
 			//循环获取处理数据，分批处理
-			dealList = selectTasks(taskItemNum, taskItemNum, eachFetchDataNum);
-			result = this.execute(dealList);
+			log.info("请求处理参数：{}, taskItemNum：{}, taskItem：{}, eachFetchDataNum：{}", taskItemNum, taskItem, eachFetchDataNum);
+			dealList = selectTasks(pramas, taskItemNum, taskItem, eachFetchDataNum);
+			if(!dealList.isEmpty()){
+				log.info("准备执行任务数据：{}", JSON.toJSONString(dealList));
+				result = this.execute(dealList);
+			}
 		}while(!dealList.isEmpty() && result);
+
 		log.info("============"+this.getClass().getName()+"任务执行结束===================");
+
 		if(result){
 			return ReturnT.SUCCESS;
 		}else{
@@ -56,12 +63,13 @@ public abstract class SettlementWorkXxlJobTaskBean<T> extends IJobHandler {
 
 	/**
 	 * 查询获取待执行任务数据
+	 * @param params 调度请求参数
 	 * @param taskItemNum 分片数量
 	 * @param taskItem 分片参数
 	 * @param eachFetchDataNum 每次处理数量 默认500
 	 * @return
 	 */
-	public abstract List<T> selectTasks(Integer taskItemNum, Integer taskItem, Integer eachFetchDataNum);
+	public abstract List<T> selectTasks(String params, Integer taskItemNum, Integer taskItem, Integer eachFetchDataNum);
 
 	/**
 	 * 处理查询到的数据

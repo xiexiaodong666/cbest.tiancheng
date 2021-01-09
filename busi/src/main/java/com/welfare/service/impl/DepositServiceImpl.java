ae83c8ef698639a0b9747407775554290bd58b5e
liyx <liyx@gillion.com.cn>
2021-01-09 16:59:32 +0800

    充值接口

---
+++ busi/src/main/java/com/welfare/service/impl/DepositServiceImpl.java
@@ -0,0 +1,32 @@
+package com.welfare.service.impl;
+
+import com.welfare.service.DepositService;
+import com.welfare.service.dto.Deposit;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Description:
+ *
+ * @author Yuxiang Li
+ * @email yuxiang.li@sjgo365.com
+ * @date 1/9/2021
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class DepositServiceImpl implements DepositService {
+
+    @Override
+    public void deposit(Deposit deposit) {
+
+    }
+
+    @Override
+    public void deposit(List<Deposit> deposits) {
+
+    }
+}

d21c10671cc015c7be9da9ebe70079897bc64a5a
殷豪 <84664373@qq.com>
2021-01-09 15:54:47 +0800

    BaseConverter 剪切到busi模块

--- service-merchant/src/main/java/com/welfare/servicemerchant/converter/MerchantAccountTypeConverter.java
+++ service-merchant/src/main/java/com/welfare/servicemerchant/converter/MerchantAccountTypeConverter.java
@@ -1,6 +1,7 @@
 package com.welfare.servicemerchant.converter;
 
 import com.welfare.persist.entity.MerchantAccountType;
+import com.welfare.service.converter.BaseConverter;
 import com.welfare.servicemerchant.dto.MerchantAccountTypeInfo;
 import org.mapstruct.Mapper;
 

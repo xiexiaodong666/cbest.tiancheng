c8b29d8e32c1b71de34e486ac8481049e24e35cb
殷豪 <84664373@qq.com>
2021-01-11 10:38:34 +0800

    商户账号类型分页接口完善

--- service-merchant/src/main/java/com/welfare/servicemerchant/dto/MerchantAccountTypeInfo.java
+++ service-merchant/src/main/java/com/welfare/servicemerchant/dto/MerchantAccountTypeInfo.java
@@ -20,47 +20,51 @@ public class MerchantAccountTypeInfo {
      * 自增id
      */
     @ApiModelProperty("自增id")
-	private Integer id;
+    private Long id;
     /**
      * 商户代码
      */
-    @ApiModelProperty("商户代码")   
+    @ApiModelProperty("商户代码")
     private String merCode;
     /**
      * 商户账户类型编码
      */
-    @ApiModelProperty("商户账户类型编码")   
+    @ApiModelProperty("商户账户类型编码")
     private String merAccountTypeCode;
     /**
      * 商户账户类型名称
      */
-    @ApiModelProperty("商户账户类型名称")   
+    @ApiModelProperty("商户账户类型名称")
     private String merAccountTypeName;
+    /**
+     * 扣款序号
+     */
+    @ApiModelProperty("扣款序号")
+    private Integer deductionOrder;
+    /**
+     * 备注
+     */
+    @ApiModelProperty("备注")
+    private String remark;
     /**
      * 创建人
      */
-    @ApiModelProperty("创建人")   
+    @ApiModelProperty("创建人")
     private String createUser;
     /**
      * 创建时间
      */
-    @ApiModelProperty("创建时间")   
+    @ApiModelProperty("创建时间")
     private Date createTime;
     /**
      * 更新人
      */
-    @ApiModelProperty("更新人")   
+    @ApiModelProperty("更新人")
     private String updateUser;
     /**
      * 更新时间
      */
-    @ApiModelProperty("更新时间")   
-	private Date updateTime;
-    /**
-     * 版本
-     */
-    @ApiModelProperty("版本")
-    private Integer version;
-
+    @ApiModelProperty("更新时间")
+    private Date updateTime;
 
 }

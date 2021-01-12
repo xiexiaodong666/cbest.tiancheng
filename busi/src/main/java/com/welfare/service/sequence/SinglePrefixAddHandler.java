070bea4c1d5f3bbd88c1c22cde321d8e6861c14c
yuxiang.li <kobe663@gmail.com>
2021-01-12 17:25:20 +0800

    序列号达到最大的处理

--- busi/src/main/java/com/welfare/service/sequence/SinglePrefixAddHandler.java
+++ busi/src/main/java/com/welfare/service/sequence/SinglePrefixAddHandler.java
@@ -1,5 +1,7 @@
 package com.welfare.service.sequence;
 
+import com.welfare.common.exception.BusiException;
+import com.welfare.common.exception.ExceptionCode;
 import com.welfare.persist.entity.Sequence;
 
 /**
@@ -9,12 +11,20 @@ import com.welfare.persist.entity.Sequence;
  * @email yuxiang.li@sjgo365.com
  * @date 1/12/2021
  */
-public class SampleHandler implements MaxSequenceExceedHandler{
+public class SinglePrefixAddHandler implements MaxSequenceExceedHandler{
+
+    public static final char Z = 'Z';
+
     @Override
     public Sequence handle(Sequence sequence) {
-        /**
-         * 样例，不做任何处理，直接返回
-         */
+        String prefix = sequence.getPrefix();
+        char[] chars = prefix.toCharArray();
+        char c = (char) (chars[0] + 1);
+        if(c > Z){
+            throw new BusiException(ExceptionCode.ILLEGALITY_ARGURMENTS,"序列号前缀达到最大",null);
+        }
+        sequence.setPrefix(String.valueOf(c));
+        sequence.setSequenceNo(sequence.getMinSequence());
         return sequence;
     }
 }

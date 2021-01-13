4a3c2eaf636073792833d485664136d54be14744
yaoxiao <314636668@qq.com>
2021-01-13 14:03:26 +0800

    updatae

--- persist/src/main/java/com/welfare/persist/dto/AccountTypeDTO.java
+++ persist/src/main/java/com/welfare/persist/dto/AccountTypeDTO.java
@@ -1,5 +1,7 @@
 package com.welfare.persist.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -8,9 +10,16 @@ import lombok.Data;
  * @date 2021/1/13 9:09
  */
 @Data
+@ApiModel("员工类型model")
 public class AccountTypeDTO {
+  @ApiModelProperty("ID")
+  private Long id;
+  @ApiModelProperty("商户编码")
   private String merCode;
+  @ApiModelProperty("员工类型编码")
   private String typeCode;
+  @ApiModelProperty("员工类型名称")
   private String typeName;
+  @ApiModelProperty("备注")
   private String remark;
 }

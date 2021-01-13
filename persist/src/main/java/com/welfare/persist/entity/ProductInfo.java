2d5dcbba17a8ecb8545cf25e644443dd1169e672
zhoujian <1131190862@qq.com>
2021-01-13 16:54:24 +0800

    添加商品查询

---
+++ persist/src/main/java/com/welfare/persist/entity/ProductInfo.java
@@ -0,0 +1,90 @@
+package com.welfare.persist.entity;
+
+import java.io.Serializable;
+
+import com.baomidou.mybatisplus.annotation.*;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * 字典(dict)实体类
+ *
+ * @author Yuxiang Li
+ * @since 2021-01-09 15:13:38
+ * @description 由 Mybatisplus Code Generator 创建
+ */
+@Data
+@NoArgsConstructor
+@Accessors(chain = true)
+@TableName("dict")
+@ApiModel("字典")
+public class Dict extends Model<Dict> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id
+     */
+    @ApiModelProperty("id")   @JsonSerialize(using = ToStringSerializer.class)
+    @TableId
+	private Long id;
+    /**
+     * 码表类型
+     */
+    @ApiModelProperty("码表类型")   
+    private String dictType;
+    /**
+     * 编码
+     */
+    @ApiModelProperty("编码")   
+    private String dictCode;
+    /**
+     * 名称
+     */
+    @ApiModelProperty("名称")   
+    private String dictName;
+    /**
+     * 状态
+     */
+    @ApiModelProperty("状态")   
+    private Integer status;
+    /**
+     * 删除标志
+     */
+    @ApiModelProperty("删除标志") @TableLogic   
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+	private Boolean deleted;
+
+//以下为列明常量
+
+    /**
+    * id
+    */
+    public static final String ID = "id";
+    /**
+    * 码表类型
+    */
+    public static final String DICT_TYPE = "dict_type";
+    /**
+    * 编码
+    */
+    public static final String DICT_CODE = "dict_code";
+    /**
+    * 名称
+    */
+    public static final String DICT_NAME = "dict_name";
+    /**
+    * 状态
+    */
+    public static final String STATUS = "status";
+    /**
+    * 删除标志
+    */
+    public static final String DELETED = "deleted";
+
+}
---
+++ persist/src/main/java/com/welfare/persist/mapper/ProductInfoMapper.java
@@ -0,0 +1,22 @@
+package com.welfare.persist.mapper;
+
+import com.welfare.persist.entity.OrderInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * (order_info)数据Mapper
+ *
+ * @author kancy
+ * @since 2021-01-12 17:25:14
+ * @description 由 Mybatisplus Code Generator 创建
+*/
+@Mapper
+public interface OrderInfoMapper extends BaseMapper<OrderInfo> {
+
+    Integer saveOrUpdate(@Param("list") List<OrderInfo> list);
+
+}

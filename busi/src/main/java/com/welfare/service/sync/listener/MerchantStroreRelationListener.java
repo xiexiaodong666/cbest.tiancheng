e4d82cd6c2bf1bc8fbec7d7867c50fd92b1de4dc
green.gao <cquptgr@gmail.com>
2021-01-14 19:48:04 +0800

    [chore] add platformUser export method

---
+++ busi/src/main/java/com/welfare/service/sync/listener/MerchantStroreRelationListener.java
@@ -0,0 +1,39 @@
+package com.welfare.service.sync.listener;
+
+
+import com.welfare.service.sync.event.MerchantAddEvt;
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.killbill.bus.api.PersistentBus;
+import org.killbill.bus.api.PersistentBus.EventBusException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+
+/**
+ * @author hao.yin
+ * @version 1.0.0
+ * @date 2020/8/5 13:16
+ */
+@Component
+@Slf4j
+public class MerchantStroreRelationListener {
+
+  @Autowired
+  PersistentBus persistentBus;
+
+  @Autowired
+  DataSource dataSource;
+
+
+  @EventListener
+  @Transactional(rollbackFor = Exception.class)
+  public void onAdd(MerchantAddEvt evt) throws EventBusException {
+    log.info("监听器。。。。{}", evt.getTest());
+
+    persistentBus.postFromTransaction(evt, DataSourceUtils.getConnection(dataSource));
+  }
+}

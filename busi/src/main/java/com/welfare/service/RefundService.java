d584148748694a4a9228728ecc264e352204955e
yuxiang.li <kobe663@gmail.com>
2021-01-15 11:09:28 +0800

    支付添加参数

---
+++ busi/src/main/java/com/welfare/service/RefundService.java
@@ -0,0 +1,8 @@
+package com.welfare.service;/**
+ *  Description:
+ *
+ *  @author Yuxiang Li
+ *  @email yuxiang.li@sjgo365.com
+ *  @date 1/15/2021
+ */public class RefundService {
+}
---
+++ busi/src/main/java/com/welfare/service/impl/RefundServiceImpl.java
@@ -0,0 +1,19 @@
+package com.welfare.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Description:
+ *
+ * @author Yuxiang Li
+ * @email yuxiang.li@sjgo365.com
+ * @date 1/15/2021
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ReductionServiceImpl {
+    
+}

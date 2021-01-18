b278800c41ac82054ddd9c9fdd2458ee7834fbe1
weiqiang <zHU3UxxD>
2021-01-18 17:39:42 +0800

    个人充值任务逻辑修改

--- service-account/src/main/java/com/welfare/serviceaccount/task/AccountDepositPaymentResultTask.java
+++ service-account/src/main/java/com/welfare/serviceaccount/task/AccountDepositPaymentResultTask.java
@@ -11,23 +11,20 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-@JobHandler("accountDepositTask")
-public class AccountDepositTask extends IJobHandler {
-
+@JobHandler("accountDepositPaymentResultTask")
+public class AccountDepositPaymentResultTask extends IJobHandler {
     private final AccountDepositRecordService accountDepositRecordService;
 
     @Override
     public ReturnT<String> execute(String s) throws Exception {
         try {
-            log.info("============{}执行支付成功后充值任务执行开始===================", this.getClass().getName());
-            accountDepositRecordService.execPendingAndFailureRechargeList();
+            log.info("============{}查询充值结果任务执行开始===================", this.getClass().getName());
+            accountDepositRecordService.execPendingPaymentList();
         } catch (Exception e) {
-            log.info("============{}执行支付成功后充值任务执行【异常】===================",
-                this.getClass().getName());
+            log.info("============{}查询充值结果任务执行【异常】===================", this.getClass().getName());
             log.error("查询充值结果执行异常", e);
             return ReturnT.FAIL;
         }
         return ReturnT.SUCCESS;
     }
-
 }

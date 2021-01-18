ae9073c300da796aa327acf229215d5ffc6c5e9c
yaoxiao <314636668@qq.com>
2021-01-18 10:36:37 +0800

    导出类型修改

--- busi/src/main/java/com/welfare/service/dto/AccountBillDetailDTO.java
+++ busi/src/main/java/com/welfare/service/dto/AccountBillDetailDTO.java
@@ -1,5 +1,7 @@
 package com.welfare.service.dto;
 
+import com.alibaba.excel.annotation.ExcelIgnoreUnannotated;
+import com.alibaba.excel.annotation.ExcelProperty;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.io.Serializable;
@@ -13,35 +15,41 @@ import lombok.Data;
  */
 @ApiModel("用户流水明细DTO")
 @Data
+@ExcelIgnoreUnannotated
 public class AccountBillDetailDTO implements Serializable {
   /**
    * 消费门店名称
    */
   @ApiModelProperty("消费门店名称")
+  @ExcelProperty(value = "消费门店名称")
   private String storeName;
 
   /**
    * 交易类型
    */
   @ApiModelProperty("交易类型")
+  @ExcelProperty(value = "交易类型(消费、退款、充值等)")
   private String transType;
 
   /**
    * 交易总金额
    */
   @ApiModelProperty("交易总金额")
+  @ExcelProperty(value = "交易总金额")
   private BigDecimal transAmount;
 
   /**
    * 账户余额
    */
   @ApiModelProperty("账户余额")
+  @ExcelProperty(value = "账户余额")
   private BigDecimal accountBalance;
 
   /**
    * 创建时间
    */
   @ApiModelProperty("创建时间")
+  @ExcelProperty(value = "创建时间")
   private String createTime;
 
 }

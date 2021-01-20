7ced3dbe287fc3ad98c809e1f57271d510bbe4c2
weiqiang <zHU3UxxD>
2021-01-20 17:57:54 +0800

    员工账户明细接口修改

--- busi/src/main/java/com/welfare/service/impl/AccountBillDetailServiceImpl.java
+++ busi/src/main/java/com/welfare/service/impl/AccountBillDetailServiceImpl.java
@@ -1,26 +1,32 @@
 package com.welfare.service.impl;
 
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.welfare.common.constants.WelfareConstant;
+import com.welfare.common.constants.WelfareConstant.Channel;
+import com.welfare.common.constants.WelfareConstant.MerAccountTypeCode;
 import com.welfare.common.constants.WelfareConstant.TransType;
 import com.welfare.persist.dao.AccountBillDetailDao;
 import com.welfare.persist.dto.AccountBillDetailSimpleDTO;
+import com.welfare.persist.dto.AccountBillDetailSimpleDeductionDTO;
 import com.welfare.persist.dto.query.AccountBillDetailSimpleReq;
 import com.welfare.persist.entity.Account;
 import com.welfare.persist.entity.AccountAmountType;
 import com.welfare.persist.entity.AccountBillDetail;
 import com.welfare.service.AccountAmountTypeService;
-import com.welfare.service.AccountBillDetailService;
 import com.welfare.service.dto.Deposit;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import com.welfare.service.AccountBillDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.Calendar;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * 用户流水明细服务接口实现
@@ -73,8 +79,34 @@ public class AccountBillDetailServiceImpl implements AccountBillDetailService {
         AccountBillDetailSimpleReq accountBillDetailSimpleReq) {
         List<AccountBillDetailSimpleDTO> accountBillDetailSimpleDTOList = accountBillDetailDao
             .getBaseMapper().selectAccountBillDetailSimpleList(accountBillDetailSimpleReq);
+
         accountBillDetailSimpleDTOList = accountBillDetailSimpleDTOList.stream()
             .map(accountBillDetailSimpleDTO -> {
+                String channel = accountBillDetailSimpleDTO.getChannel();
+                if(StrUtil.isNotEmpty(channel)) {
+                    Channel channelEnum = Channel.valueOf(channel.toUpperCase());
+                    if (channelEnum == Channel.WECHAT || channelEnum == Channel.ALIPAY) {
+                        accountBillDetailSimpleDTO.setStoreName(StrUtil
+                            .format("{}({})", accountBillDetailSimpleDTO.getStoreName(),
+                                channelEnum.desc()));
+                    }
+                }
+
+                List<AccountBillDetailSimpleDeductionDTO> deductionList = accountBillDetailSimpleDTO
+                    .getDeductionList();
+                if(CollectionUtil.isNotEmpty(deductionList)) {
+
+                    Optional<AccountBillDetailSimpleDeductionDTO> optional = deductionList.stream()
+                        .filter(
+                            deductionDTO -> (
+                                MerAccountTypeCode.SELF.code()
+                                    .equals(deductionDTO.getMerAccountTypeCode())))
+                        .findFirst();
+
+                    if (!optional.isPresent()) {
+                        accountBillDetailSimpleDTO.setDeductionList(null);
+                    }
+                }
                 accountBillDetailSimpleDTO.setTransTypeName(TransType
                     .valueOf(accountBillDetailSimpleDTO.getTransType().toUpperCase()).desc());
                 return accountBillDetailSimpleDTO;

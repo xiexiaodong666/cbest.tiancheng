01606bcd422343158a3714a2b594937175311e48
green.gao <cquptgr@gmail.com>
2021-01-20 17:39:35 +0800

    [update] 修复卡片查询时间, 门店修改后同步到 消费门店，也需要同步到商户

--- busi/src/main/java/com/welfare/service/impl/SupplierStoreServiceImpl.java
+++ busi/src/main/java/com/welfare/service/impl/SupplierStoreServiceImpl.java
@@ -59,6 +59,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;

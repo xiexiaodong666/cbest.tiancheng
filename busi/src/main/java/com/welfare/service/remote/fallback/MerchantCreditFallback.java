c8e53e5bc3020a0f2b0742fe991474e6bf253d97
dengqiang <17783863034@163.com>
2021-01-22 21:59:45 +0800

    授信额度需要结算

--- busi/src/main/java/com/welfare/service/remote/fallback/MerchantCreditFallback.java
+++ busi/src/main/java/com/welfare/service/remote/fallback/MerchantCreditFallback.java
@@ -4,7 +4,9 @@ import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
 import com.welfare.common.exception.BusiException;
 import com.welfare.common.exception.ExceptionCode;
+import com.welfare.service.dto.RestoreRemainingLimitReq;
 import com.welfare.service.remote.MerchantCreditFeign;
+import com.welfare.service.remote.entity.MerchantCreditResp;
 import feign.hystrix.FallbackFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -20,9 +22,12 @@ public class MerchantCreditFallback implements FallbackFactory<MerchantCreditFei
 
   @Override
   public MerchantCreditFeign create(Throwable throwable) {
-    return req -> {
-      log.error(StrUtil.format("调用商户恢复商户信用额度异常, req: {}", JSON.toJSONString(req)), throwable);
-      throw new BusiException(ExceptionCode.UNKNOWON_EXCEPTION, "系统异常", null);
+    return new MerchantCreditFeign() {
+      @Override
+      public MerchantCreditResp remainingLimit(RestoreRemainingLimitReq request, String source) {
+        log.error(StrUtil.format("调用商户恢复商户信用额度异常, req: {}", JSON.toJSONString(request)), throwable);
+        throw new BusiException(ExceptionCode.UNKNOWON_EXCEPTION, "系统异常", null);
+      }
     };
   }
 }

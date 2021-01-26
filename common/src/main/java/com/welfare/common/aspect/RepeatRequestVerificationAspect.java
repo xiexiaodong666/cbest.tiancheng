affa485f78cfce1d09938dcd6e0a47cba2b42449
yaoxiao <314636668@qq.com>
2021-01-26 17:08:31 +0800

    update

--- common/src/main/java/com/welfare/common/aspect/RepeatRequestVerificationAspect.java
+++ common/src/main/java/com/welfare/common/aspect/RepeatRequestVerificationAspect.java
@@ -1,34 +1,22 @@
 package com.welfare.common.aspect;
 
-import com.welfare.common.annotation.DistributedLock;
 import com.welfare.common.annotation.RepeatRequestVerification;
 import com.welfare.common.exception.BusiException;
 import com.welfare.common.exception.ExceptionCode;
-import com.welfare.common.util.DistributedLockUtil;
-import java.lang.reflect.Parameter;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import net.dreamlu.mica.core.utils.JsonUtil;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.expression.EvaluationContext;
-import org.springframework.expression.Expression;
-import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.common.TemplateParserContext;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
-import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 @RequiredArgsConstructor
 public class RepeatRequestVerificationAspect {
-  private final RedissonClient redissonClient;
   private final StringRedisTemplate stringRedisTemplate;
 
   @Around("@annotation(repeatRequestVerification)")

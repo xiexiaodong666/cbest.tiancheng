f5cbd1b7e4335a9ae0482ea2bc1d1c088defcea8
zhoujian <1131190862@qq.com>
2021-01-26 23:07:07 +0800

    调整订单查询-根据流水为主数据

--- persist/src/main/java/com/welfare/persist/mapper/OrderInfoMapper.java
+++ persist/src/main/java/com/welfare/persist/mapper/OrderInfoMapper.java
@@ -2,6 +2,7 @@ package com.welfare.persist.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.welfare.persist.dto.OrderInfoDTO;
 import com.welfare.persist.dto.query.OrderPageQuery;
 import com.welfare.persist.entity.OrderInfo;
 import com.welfare.persist.entity.OrderSummary;
@@ -22,9 +23,9 @@ public interface OrderInfoMapper extends BaseMapper<OrderInfo> {
 
     Integer saveOrUpdate(@Param("list") List<OrderInfo> list);
 
-    Page<OrderInfo> searchOrder(Page page , @Param("orderPageQuery") OrderPageQuery orderPageQuery);
+    Page<OrderInfoDTO> searchOrder(Page page , @Param("orderPageQuery") OrderPageQuery orderPageQuery);
 
-    List<OrderInfo> searchOrder(@Param("orderPageQuery") OrderPageQuery orderPageQuery);
+    List<OrderInfoDTO> searchOrder(@Param("orderPageQuery") OrderPageQuery orderPageQuery);
 
     OrderSummary searchOrderSum(@Param("orderPageQuery") OrderPageQuery orderPageQuery);
 }

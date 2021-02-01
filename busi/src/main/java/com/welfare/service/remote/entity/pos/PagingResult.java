f06b9b2bb8d79b06e309e4f812a36d6a177fd90f
duanhy <dhongyuan@foxmail.com>
2021-02-01 14:48:29 +0800

    去掉多余的包引用

--- busi/src/main/java/com/welfare/service/remote/entity/pos/PagingResult.java
+++ busi/src/main/java/com/welfare/service/remote/entity/pos/PagingResult.java
@@ -1,5 +1,6 @@
 package com.welfare.service.remote.entity.pos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.util.List;
  */
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class PagingResult<T> {
   private Integer pageNo;
   private Integer pageSize;

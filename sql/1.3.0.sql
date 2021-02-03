5440f31c38d1be3928433fa9e943c5ca71ad9010
yuxiang.li <kobe663@gmail.com>
2021-02-03 16:05:54 +0800

    流水记录条码或者磁卡信息

--- sql/1.3.0.sql
+++ sql/1.3.0.sql
@@ -1,2 +1,7 @@
+# 条码加盐表添加索引
 create index idx_bs_valid_period on barcode_salt(valid_period);
 create index idx_bs_valid_period_numeric on barcode_salt(valid_period_numeric);
+
+# 流水中记录条码信息或者磁卡信息
+alter table account_bill_detail add column payment_type varchar(20) comment '支付方式:条码或者线上';
+alter table account_bill_detail add column payment_type_info varchar(20) comment '支付方式信息:条码或磁卡信息';

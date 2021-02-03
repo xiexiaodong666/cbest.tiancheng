1c70b891c68d92b377beb9fab49d77c848d8d32f
yuxiang.li <kobe663@gmail.com>
2021-02-03 16:23:22 +0800

    支付条码记录sql修改

--- sql/1.3.0.sql
+++ sql/1.3.0.sql
@@ -3,5 +3,5 @@ create index idx_bs_valid_period on barcode_salt(valid_period);
 create index idx_bs_valid_period_numeric on barcode_salt(valid_period_numeric);
 
 # 流水中记录条码信息或者磁卡信息
-alter table account_bill_detail add column payment_type varchar(20) comment '支付方式:条码或者线上';
-alter table account_bill_detail add column payment_type_info varchar(20) comment '支付方式信息:条码或磁卡信息';
+alter table account_bill_detail add column payment_type varchar(20) comment '支付方式:条码或者线上' after order_channel;
+alter table account_bill_detail add column payment_type_info varchar(64) comment '支付方式信息:条码或磁卡信息' after payment_type;

36145cdaa3ff89cbb5275820dcbb226a7a1e01ee
yuxiang.li <kobe663@gmail.com>
2021-02-03 13:49:47 +0800

    支付条码不能重复

--- busi/src/main/java/com/welfare/service/dto/payment/BarcodePaymentRequest.java
+++ busi/src/main/java/com/welfare/service/dto/payment/BarcodePaymentRequest.java
@@ -1,13 +1,20 @@
 package com.welfare.service.dto.payment;
 
+import com.welfare.common.exception.BusiException;
+import com.welfare.common.exception.ExceptionCode;
 import com.welfare.common.util.SpringBeanUtils;
+import com.welfare.common.util.StringUtil;
 import com.welfare.service.BarcodeService;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.util.StringUtils;
 
 import java.util.Date;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Description:
@@ -19,9 +26,9 @@ import java.util.Objects;
 @ApiModel("扫码支付请求")
 @Data
 public class BarcodePaymentRequest extends PaymentRequest {
-    @ApiModelProperty("条码")
+    @ApiModelProperty(value = "条码",required = true)
     private String barcode;
-    @ApiModelProperty("扫描日期，yyyy-MM-ddTHH:mm:ss+08:00,例:2021-01-01T12:00:00+08:00")
+    @ApiModelProperty(value = "扫描日期，yyyy-MM-ddTHH:mm:ss+08:00,例:2021-01-01T12:00:00+08:00",required = true)
     private Date scanDate;
 
     @Override
@@ -30,6 +37,16 @@ public class BarcodePaymentRequest extends PaymentRequest {
             return super.getAccountCode();
         }
         BarcodeService barcodeService = SpringBeanUtils.getBean(BarcodeService.class);
+        RedisTemplate<String,String> redisTemplate = SpringBeanUtils.getBean(StringRedisTemplate.class);
+        String expireSecs = SpringBeanUtils.getApplicationContext()
+                .getEnvironment()
+                .getProperty("e-welfare.barcode.expire", "180");
+        String barcodeInRedis = redisTemplate.opsForValue().get("BARCODE:" + barcode);
+        if(!StringUtils.isEmpty(barcodeInRedis)){
+            throw new BusiException(ExceptionCode.ILLEGALITY_ARGURMENTS,"重复的支付条码",null);
+        }else{
+            redisTemplate.opsForValue().set("BARCODE:"+barcode,barcode,Long.parseLong(expireSecs), TimeUnit.SECONDS);
+        }
         Long accountCode = barcodeService.parseAccountFromBarcode(barcode, scanDate,getOffline());
         this.setAccountCode(accountCode);
         return accountCode;

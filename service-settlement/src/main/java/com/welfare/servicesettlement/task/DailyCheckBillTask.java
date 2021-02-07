f3841e15d960bf5dc32194e4adeb8e280c79ab24
yuxiang.li <kobe663@gmail.com>
2021-02-07 14:41:02 +0800

    checkbill

---
+++ service-settlement/src/main/java/com/welfare/servicesettlement/task/DailyCheckBillTask.java
@@ -0,0 +1,8 @@
+package com.welfare.servicesettlement.task;/**
+ *  Description:
+ *
+ *  @author Yuxiang Li
+ *  @email yuxiang.li@sjgo365.com
+ *  @date 2/7/2021
+ */public class DailyCheckBillTask {
+}

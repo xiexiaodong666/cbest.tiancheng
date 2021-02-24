ed09e7a60d0753912c306d5903de921157353832
yuxiang.li <kobe663@gmail.com>
2021-02-24 09:57:51 +0800

    settle detail add properties and params

--- busi/src/main/java/com/welfare/service/dto/WelfareSettleDetailResp.java
+++ busi/src/main/java/com/welfare/service/dto/WelfareSettleDetailResp.java
@@ -71,4 +71,16 @@ public class WelfareSettleDetailResp {
     @ExcelProperty(value = "结算金额")
     @ApiModelProperty(value = "结算金额")
     private String settleAmount;
+
+    @ExcelProperty(value = "结算状态")
+    @ApiModelProperty(value = "结算状态")
+    private String settleFlag;
+
+    @ExcelProperty(value = "手机号")
+    @ApiModelProperty(value = "手机号")
+    private String phone;
+
+    @ExcelProperty(value = "部门名称")
+    @ApiModelProperty(value = "部门名称")
+    private String departmentName;
 }

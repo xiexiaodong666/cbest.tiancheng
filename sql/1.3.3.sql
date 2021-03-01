5eb80c88febefb821a64066990082595e05c568d
duanhy <dhongyuan@foxmail.com>
2021-03-01 11:42:21 +0800

    新增sql

--- sql/1.3.3.sql
+++ sql/1.3.3.sql
@@ -6,13 +6,13 @@ INSERT INTO `dict` (`id`, `dict_type`, `dict_code`, `dict_name`, `status`, `dele
 INSERT INTO `dict` (`id`, `dict_type`, `dict_code`, `dict_name`, `status`, `deleted`, `sort`) VALUES ('80', 'SettleFlag', 'settled', '已结算', '1', '0', '3');
 
 #两张表添加order_channel
-alter table account_deduction_detail add column order_channel varchar(20) comment '订单渠道' after chanel;
+alter table account_deduction_detaPaymentControlleril add column order_channel varchar(20) comment '订单渠道' after chanel;
 alter table settle_detail add column order_channel varchar(20) comment '订单渠道' after settle_flag;
 #上面两个新加字段的历史数据补充
 update account_deduction_detail d set d.order_channel = (select order_channel from account_bill_detail abd where abd.trans_no = d.trans_no limit 1);
 update settle_detail d set d.order_channel = (select order_channel from account_bill_detail abd where abd.trans_no = d.trans_no limit 1);
 
-#新增字典类型 批发收货地址、批发商城
+#新增字典类型 批发收货地址 批发商城
 INSERT INTO `dict`(`id`, `dict_type`, `dict_code`, `dict_name`, `status`, `deleted`, `sort`) VALUES (81, 'MerchantAddress.addressType', 'WHOLESALE', '批发收货地址', 1, 0, 3);
 INSERT INTO `dict`(`id`, `dict_type`, `dict_code`, `dict_name`, `status`, `deleted`, `sort`) VALUES (82, 'SupplierStore.consumType', 'WHOLESALE', '批发商城', 1, 0, 4);
 

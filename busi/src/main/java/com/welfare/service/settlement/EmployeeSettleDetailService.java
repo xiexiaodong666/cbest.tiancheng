008628d1294561fa7032b968e0ed6c38184d488d
yuxiang.li <kobe663@gmail.com>
2021-03-03 17:48:58 +0800

    employee_settle code generate

---
+++ busi/src/main/java/com/welfare/service/settlement/EmployeeSettleDetailService.java
@@ -0,0 +1,13 @@
+package com.welfare.service.settlement;
+
+
+/**
+ * 服务接口
+ *
+ * @author Yuxiang Li
+ * @since 2021-03-03 17:44:04
+ * @description 由 Mybatisplus Code Generator 创建
+ */
+public interface EmployeeSettleDetailService {
+
+}
---
+++ persist/src/main/java/com/welfare/persist/mapper/EmployeeSettleDetailMapper.java
@@ -0,0 +1,17 @@
+package com.welfare.persist.mapper;
+
+import com.welfare.persist.entity.EmployeeSettleDetail;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * (employee_settle_detail)数据Mapper
+ *
+ * @author Yuxiang Li
+ * @since 2021-03-03 17:44:04
+ * @description 由 Mybatisplus Code Generator 创建
+*/
+@Mapper
+public interface EmployeeSettleDetailMapper extends BaseMapper<EmployeeSettleDetail> {
+
+}

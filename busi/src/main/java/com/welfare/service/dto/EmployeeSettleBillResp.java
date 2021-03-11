8e08e9d917ba83c0c1fc0d9092ed92140c82a281
huangrongya <huangrongya@foxmail.com>
2021-03-11 16:14:00 +0800

    个人授信消费账单返回结算周期

--- busi/src/main/java/com/welfare/service/dto/EmployeeSettleBillResp.java
+++ busi/src/main/java/com/welfare/service/dto/EmployeeSettleBillResp.java
@@ -53,6 +53,9 @@ public class EmployeeSettleBillResp {
     @ApiModelProperty(value = "账单结算金额")
     private String settleAmount;
 
+    @ApiModelProperty(value = "账单结算周期")
+    private String settlePeriod;
+
     @ApiModelProperty(value = "结算状态:待结算-unsettled 已结算-settled")
     private String settleStatus;
 
--- persist/src/main/java/com/welfare/persist/dto/EmployeeSettleBillDTO.java
+++ persist/src/main/java/com/welfare/persist/dto/EmployeeSettleBillDTO.java
@@ -41,6 +41,8 @@ public class EmployeeSettleBillDTO {
 
     private Date settleTime;
 
+    private String settlePeriod;
+
 
 }
 

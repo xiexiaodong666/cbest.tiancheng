0f81302b2133f37b6f931cf64105c10916ef2e59
yuxiang.li <kobe663@gmail.com>
2021-03-12 09:20:11 +0800

    条码加盐逻辑优化

--- busi/src/main/java/com/welfare/service/impl/BarcodeServiceImpl.java
+++ busi/src/main/java/com/welfare/service/impl/BarcodeServiceImpl.java
@@ -125,10 +125,16 @@ public class BarcodeServiceImpl implements BarcodeService {
     public BarcodeSalt querySaltByPeriodNumeric(Long period) {
         QueryWrapper<BarcodeSalt> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq(BarcodeSalt.VALID_PERIOD, period);
-        return barcodeSaltDao.getOne(queryWrapper);
+        BarcodeSalt barcodeSalt = barcodeSaltDao.getOne(queryWrapper);
+        if(Objects.isNull(barcodeSalt)){
+            batchGenerateSalt();
+            barcodeSalt = barcodeSaltDao.getOne(queryWrapper);
+        }
+        return barcodeSalt;
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public PaymentBarcode getBarcode(Long accountCode) {
         BarcodeSalt barcodeSalt =  queryPeriodSaltValue(Calendar.getInstance().getTime());
         PaymentBarcode paymentBarcode = PaymentBarcode.of(accountCode, barcodeSalt.getSaltValue());

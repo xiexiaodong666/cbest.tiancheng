259a8cba9b0b6e07d3c962e92850d3a9f681d2be
yuxiang.li <kobe663@gmail.com>
2021-03-12 19:01:09 +0800

    deploy.sh

---
+++ service-account/deploy131.sh
@@ -0,0 +1,15 @@
+#mvn clean install
+cd ..
+git pull
+mvn clean install -DskipTests
+cd ./service-account || exit
+scp ./target/e-welfare-account.jar root@172.30.37.131:/home/finance/App/e-welfare-account.e-cbest.lotest/release/
+echo "sleeping 3 secs"
+sleep 3
+  ssh root@172.30.37.131 "
+chown  -R  finance:finance /home/finance/App/e-welfare-account.e-cbest.lotest/release;
+sh /home/finance/app-shell/run.sh stop e-welfare-account;
+sleep 5;
+sh /home/finance/app-shell/run.sh start e-welfare-account;
+tail -f /home/finance/Logs/e-welfare-account.e-cbest.lotest/console.log
+"

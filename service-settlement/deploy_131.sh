2be5b6ea5a0c9c16e6c4b0f8faa904a1d8e17d08
duanhy <dhongyuan@foxmail.com>
2021-03-15 14:06:13 +0800

    新增入参支付渠道

---
+++ service-settlement/deploy_131.sh
@@ -0,0 +1,13 @@
+git pull
+cd ..
+mvn clean install -DskipTests
+cd ./service-settlement
+scp ./target/e-welfare-settlement.jar root@172.30.37.131:/home/finance/App/e-welfare-settlement.e-cbest.lotest/release/
+echo "sleeping 3 secs"
+sleep 3
+  ssh root@172.30.37.131 "
+chown  -R  finance:finance /home/finance/App/e-welfare-settlement.e-cbest.lotest/release;
+sh /home/finance/app-shell/run.sh stop e-welfare-settlement;
+sleep 5;
+sh /home/finance/app-shell/run.sh start e-welfare-settlement;
+"

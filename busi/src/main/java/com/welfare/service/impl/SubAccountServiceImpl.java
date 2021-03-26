21061401e85351aa6dd61bcc493a6dea9d24c8e7
yuxiang.li <kobe663@gmail.com>
2021-03-26 11:53:40 +0800

    维护免密支付签名

---
+++ busi/src/main/java/com/welfare/service/impl/SubAccountServiceImpl.java
@@ -0,0 +1,28 @@
+package com.welfare.service.impl;
+
+import com.welfare.persist.dao.SubAccountDao;
+import com.welfare.persist.entity.SubAccount;
+import com.welfare.service.SubAccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+/**
+ * Description:
+ *
+ * @author Yuxiang Li
+ * @email yuxiang.li@sjgo365.com
+ * @date 3/26/2021
+ */
+@Service
+@RequiredArgsConstructor
+public class SubAccountServiceImpl implements SubAccountService {
+    private final SubAccountDao subAccountDao;
+    @Override
+    public SubAccount passwordFreeSignature(Long accountCode, String paymentChannel, String passwordFreeSignature) {
+        SubAccount subAccount = subAccountDao.getByAccountCodeAndType(accountCode, paymentChannel);
+        subAccount.setPasswordFreeSignature(passwordFreeSignature);
+        subAccountDao.updateById(subAccount);
+        return subAccount;
+    }
+}

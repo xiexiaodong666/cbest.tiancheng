13af2a6a58ed6f4edd9e4ab8738604e73d7fac43
duanhy <dhongyuan@foxmail.com>
2021-03-31 11:37:25 +0800

    新增 商户-手机号-删除标志的联合唯一索引

--- sql/1.5.1.sql
+++ sql/1.5.1.sql
@@ -8,3 +8,6 @@ UPDATE merchant SET bill_detail_show_store_name='1';
 
 INSERT INTO `dict`(`dict_type`, `dict_code`, `dict_name`, `status`, `deleted`, `sort`) VALUES ('Merchant.billDetailShowStoreName', '1', '显示', NULL, 0, 1);
 INSERT INTO `dict`(`dict_type`, `dict_code`, `dict_name`, `status`, `deleted`, `sort`) VALUES ('Merchant.billDetailShowStoreName', '0', '不显示', NULL, 0, 2);
+
+# account 新增 商户-手机号-删除标志的联合唯一索引
+alter table account add unique index uk_mer_code_phone_deleted(mer_code,phone,deleted);

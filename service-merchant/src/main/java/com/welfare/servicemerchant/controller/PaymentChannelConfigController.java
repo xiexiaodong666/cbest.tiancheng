21b7922c08b6bdac15b9a2257b04c7a36038d856
duanhy <dhongyuan@foxmail.com>
2021-04-12 14:16:09 +0800

    根据商户、门店、消费场景查询支付渠道配置

--- service-merchant/src/main/java/com/welfare/servicemerchant/controller/PaymentChannelConfigController.java
+++ service-merchant/src/main/java/com/welfare/servicemerchant/controller/PaymentChannelConfigController.java
@@ -1,6 +1,8 @@
 package com.welfare.servicemerchant.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.welfare.common.annotation.MerchantUser;
+import com.welfare.persist.entity.PaymentChannelConfig;
 import com.welfare.service.PaymentChannelConfigService;
 import com.welfare.service.dto.paymentChannel.*;
 import io.swagger.annotations.Api;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @Author: duanhy
@@ -46,5 +50,12 @@ public class PaymentChannelConfigController {
   @ApiOperation("编辑商户所选消费方式的支付渠道")
   public R<Boolean> edit(@Validated @RequestBody PayChannelConfigEditReq req) {
     return R.success(channelConfigService.edit(req));
+  }
+
+  @PostMapping("/getByCondition")
+  @ApiOperation("通过商户编码、门店编码、消费场景查询")
+  @MerchantUser
+  public R<List<PaymentChannelConfig>> get(@Validated @RequestBody PaymentChannelConfigReq req) {
+    return R.success(channelConfigService.getByMerCodeAndStoreAndConsume(req));
   }
 }

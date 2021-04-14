f960660f04e51f5b8e6399829bc4a95f2f3476dc
yuxiang.li <kobe663@gmail.com>
2021-04-14 11:59:08 +0800

    查询分组

--- busi/src/main/java/com/welfare/service/AccountAmountTypeGroupService.java
+++ busi/src/main/java/com/welfare/service/AccountAmountTypeGroupService.java
@@ -1,6 +1,7 @@
 package com.welfare.service;
 
 
+import com.welfare.persist.entity.AccountAmountTypeGroup;
 import com.welfare.service.dto.nhc.NhcFamilyMemberDTO;
 
 /**
@@ -10,23 +11,29 @@ import com.welfare.service.dto.nhc.NhcFamilyMemberDTO;
  */
 public interface AccountAmountTypeGroupService {
 
-  /**
-   * 移除员工
-   * @param accountCode
-   * @param merAccountTypeCode
-   * @return
-   */
-  boolean removeByAccountCode(Long accountCode, String merAccountTypeCode);
+    /**
+     * 移除员工
+     * @param accountCode
+     * @param merAccountTypeCode
+     * @return
+     */
+    boolean removeByAccountCode(Long accountCode, String merAccountTypeCode);
 
-  /**
-   * 加入组
-   * @param joinAccountCode 新加入的员工编码
-   * @param groupAccountCode 组内的某个员工的编码
-   * @param merAccountTypeCode 福利类型
-   * @return
-   */
-  boolean addByAccountCodeAndMerAccountTypeCode(Long joinAccountCode, Long groupAccountCode, String merAccountTypeCode);
+    /**
+     * 加入组
+     * @param joinAccountCode 新加入的员工编码
+     * @param groupAccountCode 组内的某个员工的编码
+     * @param merAccountTypeCode 福利类型
+     * @return
+     */
+    boolean addByAccountCodeAndMerAccountTypeCode(Long joinAccountCode, Long groupAccountCode, String merAccountTypeCode);
 
+    /**
+     * 根据账户号查询组
+     * @param accountCode
+     * @return
+     */
+    AccountAmountTypeGroup queryByAccountCode(Long accountCode);
 
 
 }
--- busi/src/main/java/com/welfare/service/impl/AccountAmountTypeGroupServiceImpl.java
+++ busi/src/main/java/com/welfare/service/impl/AccountAmountTypeGroupServiceImpl.java
@@ -1,16 +1,23 @@
 package com.welfare.service.impl;
 
+import com.welfare.common.constants.WelfareConstant;
 import com.welfare.common.exception.BizAssert;
 import com.welfare.common.exception.ExceptionCode;
 import com.welfare.persist.dao.AccountAmountTypeDao;
+import com.welfare.persist.dao.AccountAmountTypeGroupDao;
+import com.welfare.persist.dao.AccountDao;
 import com.welfare.persist.entity.Account;
 import com.welfare.persist.entity.AccountAmountType;
+import com.welfare.persist.entity.AccountAmountTypeGroup;
 import com.welfare.service.AccountAmountTypeGroupService;
 import com.welfare.service.AccountAmountTypeService;
 import com.welfare.service.AccountService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 /**
  * @Author: duanhy
@@ -19,30 +26,44 @@ import org.springframework.stereotype.Service;
  */
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class AccountAmountTypeGroupServiceImpl implements AccountAmountTypeGroupService {
 
-  @Autowired
-  private AccountService accountService;
-  @Autowired
-  private AccountAmountTypeService accountAmountTypeService;
-  @Autowired
-  private AccountAmountTypeDao accountAmountTypeDao;
+    @Autowired
+    private AccountDao accountDao;
+    @Autowired
+    private AccountAmountTypeService accountAmountTypeService;
+    @Autowired
+    private AccountAmountTypeDao accountAmountTypeDao;
+    private final AccountAmountTypeGroupDao accountAmountTypeGroupDao;
+    @Override
+    public boolean removeByAccountCode(Long accountCode, String merAccountTypeCode) {
+        Account account = accountDao.queryByAccountCode(accountCode);
+        BizAssert.notNull(account, ExceptionCode.ILLEGALITY_ARGURMENTS, "用户不存在");
+        AccountAmountType accountAmountType = accountAmountTypeService.queryOne(accountCode, merAccountTypeCode);
+        BizAssert.notNull(accountAmountType, ExceptionCode.ILLEGALITY_ARGURMENTS, "福利类型为空");
+        BizAssert.isTrue(accountAmountType.getJoinedGroup() != null && accountAmountType.getJoinedGroup(),
+                ExceptionCode.ILLEGALITY_ARGURMENTS, "该用户没加入任何组");
+        accountAmountType.setJoinedGroup(Boolean.FALSE);
+        accountAmountType.setAccountAmountTypeGroupId(null);
+        return accountAmountTypeDao.updateById(accountAmountType);
+    }
 
-  @Override
-  public boolean removeByAccountCode(Long accountCode, String merAccountTypeCode) {
-    Account account = accountService.getByAccountCode(accountCode);
-    BizAssert.notNull(account, ExceptionCode.ILLEGALITY_ARGURMENTS, "用户不存在");
-    AccountAmountType accountAmountType = accountAmountTypeService.queryOne(accountCode, merAccountTypeCode);
-    BizAssert.notNull(accountAmountType, ExceptionCode.ILLEGALITY_ARGURMENTS, "福利类型为空");
-    BizAssert.isTrue(accountAmountType.getJoinedGroup() != null && accountAmountType.getJoinedGroup(),
-            ExceptionCode.ILLEGALITY_ARGURMENTS, "该用户没加入任何组");
-    accountAmountType.setJoinedGroup(Boolean.FALSE);
-    accountAmountType.setAccountAmountTypeGroupId(null);
-    return accountAmountTypeDao.updateById(accountAmountType);
-  }
+    @Override
+    public boolean addByAccountCodeAndMerAccountTypeCode(Long joinAccountCode, Long groupAccountCode, String merAccountTypeCode) {
+        return false;
+    }
 
-  @Override
-  public boolean addByAccountCodeAndMerAccountTypeCode(Long joinAccountCode, Long groupAccountCode, String merAccountTypeCode) {
-    return false;
-  }
+    @Override
+    public AccountAmountTypeGroup queryByAccountCode(Long accountCode) {
+        AccountAmountType accountAmountType = accountAmountTypeDao.queryByAccountCodeAndAmountType(
+                accountCode,
+                WelfareConstant.MerAccountTypeCode.MALL_POINT.code()
+        );
+        if (Objects.nonNull(accountAmountType) && accountAmountType.getJoinedGroup()) {
+            return accountAmountTypeGroupDao.getById(accountAmountType.getAccountAmountTypeGroupId());
+        }else{
+            return null;
+        }
+    }
 }

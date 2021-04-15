84ef9c7545fd7d76347f572b2e8d69f3e25de460
green.gao <cquptgr@gmail.com>
2021-04-15 13:48:14 +0800

    [dev-2.1.3 UPDATE] 报表导出设置默认宽度

--- busi/src/main/java/com/welfare/service/dto/merchantconsume/ExportMerChantConsumeData.java
+++ busi/src/main/java/com/welfare/service/dto/merchantconsume/ExportMerChantConsumeData.java
@@ -1,6 +1,7 @@
 package com.welfare.service.dto.merchantconsume;
 
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import com.welfare.service.dto.merchantconsume.WelfareMerChantConsumeDataApiResponse.TableExt;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -15,58 +16,72 @@ public class ExportMerChantConsumeData {
 
   @ApiModelProperty("商户代码")
   @ExcelProperty(value = "商户代码")
+  @ColumnWidth(10)
   private String merCode;
 
   @ApiModelProperty("商户名称")
   @ExcelProperty(value = "商户名称")
+  @ColumnWidth(10)
   private String merName;
 
   @ApiModelProperty("员工人数")
   @ExcelProperty(value = "员工人数")
+  @ColumnWidth(10)
   private String userNum;
 
   @ApiModelProperty("商户余额")
   @ExcelProperty(value = "商户余额")
+  @ColumnWidth(10)
   private String currentBalance;
 
   @ApiModelProperty("剩余信用额度/信用额度")
   @ExcelProperty(value = "剩余信用额度/信用额度")
+  @ColumnWidth(10)
   private String remainingLimit;
 
   @ApiModelProperty("结算额度")
   @ExcelProperty(value = "结算额度")
+  @ColumnWidth(10)
   private String settledMoney;
 
   @ApiModelProperty("待结算额度")
   @ExcelProperty(value = "待结算额度")
+  @ColumnWidth(10)
   private String unsettledMoney;
 
   @ApiModelProperty("业务类型")
   @ExcelProperty(value = "业务类型")
+  @ColumnWidth(30)
   private String businessType;
 
   @ApiModelProperty("消费方式")
   @ExcelProperty(value = "消费方式")
+  @ColumnWidth(30)
   private String consumeType;
 
   @ApiModelProperty("消费总金额")
   @ExcelProperty(value = "消费总金额")
+  @ColumnWidth(10)
   private Double consumeMoney;
 
   @ApiModelProperty("消费人数")
   @ExcelProperty(value = "消费人数")
+  @ColumnWidth(10)
   private Integer consumePeopleNum;
 
   @ApiModelProperty("交易笔数")
   @ExcelProperty(value = "交易笔数")
+  @ColumnWidth(10)
   private Integer transNum;
 
   @ApiModelProperty("人均消费")
   @ExcelProperty(value = "人均消费")
+  @ColumnWidth(10)
   private Double avgPeopleConsumeMoney;
 
   @ApiModelProperty("每笔平均交易额")
   @ExcelProperty(value = "每笔平均交易额")
+  @ColumnWidth(10)
   private Double avgTransMoney;
 
   public static ExportMerChantConsumeData rowsOf(MerChantConsumeDataRowsApiResponse response) {

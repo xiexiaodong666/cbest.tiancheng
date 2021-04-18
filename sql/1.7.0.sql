f35d3059358c5d151f05cbd4cd73b16998bd8949
duanhy <duan14789632>
2021-04-18 12:50:06 +0800

    sql

--- sql/1.7.0.sql
+++ sql/1.7.0.sql
@@ -28,19 +28,21 @@ CREATE TABLE `merchant_extend` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='商户扩展表';
 
 CREATE TABLE `mer_account_type_consume_scene_config` (
-                                                         `id` bigint(20) NOT NULL COMMENT 'id',
-                                                         `mer_code` bigint(20) NOT NULL COMMENT '商户编码',
-                                                         `mer_account_type_code` varchar(20) NOT NULL COMMENT '福利类型',
-                                                         `store_code` varchar(20) NOT NULL COMMENT '门店编码',
-                                                         `scene_consume_type` varchar(255) NOT NULL COMMENT '消费方式',
-                                                         `create_user` varchar(50) DEFAULT NULL COMMENT '创建人',
-                                                         `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-                                                         `update_user` varchar(50) DEFAULT NULL COMMENT '更新人',
-                                                         `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-                                                         `deleted` tinyint(1) DEFAULT NULL COMMENT '删除标志  1-删除、0-未删除',
-                                                         `version` int(11) DEFAULT NULL COMMENT '版本',
-                                                         PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='福利类型消费门店场景配置';
+                                  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+                                  `mer_code` varchar(20) NOT NULL COMMENT '商户编码',
+                                  `mer_account_type_code` varchar(20) NOT NULL COMMENT '福利类型',
+                                  `store_code` varchar(20) NOT NULL COMMENT '门店编码',
+                                  `scene_consume_type` varchar(255) NOT NULL COMMENT '消费方式',
+                                  `create_user` varchar(50) DEFAULT NULL COMMENT '创建人',
+                                  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+                                  `update_user` varchar(50) DEFAULT NULL COMMENT '更新人',
+                                  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+                                  `deleted` bigint(20) DEFAULT NULL COMMENT '删除标志',
+                                  `version` int(11) DEFAULT NULL COMMENT '版本',
+                                  PRIMARY KEY (`id`),
+                                  UNIQUE KEY `uk_merCode_merAccountTypeCode_storeCode_sceneConsumeType_deleted` (`mer_code`,`mer_account_type_code`,`store_code`,`scene_consume_type`,`deleted`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='福利类型消费门店场景配置';
+
 
 alter table account_bill_detail add column account_amount_type_group_id bigint(20) comment '福利类型组id' after payment_channel;
 alter table account_deduction_detail add column account_amount_type_group_id bigint(20) comment '福利类型组id' after payment_channel;
@@ -51,4 +53,56 @@ alter table account_amount_type add column account_amount_type_group_id bigint(2
 
 # DML ---
 
+#  福利类型 o2o 配置
+INSERT INTO `mer_account_type_consume_scene_config`(`mer_code`, `mer_account_type_code`, `store_code`, `scene_consume_type`, `create_user`, `create_time`, `update_user`, `update_time`, `deleted`, `version`)
+SELECT a.mer_code,mac.mer_account_type_code,a.store_code,'O2O','duanhy', now(), NULL, NULL, 0 ,0
+from
+merchant_account_type mac
+LEFT JOIN
+(
+SELECT msr.id, msr.mer_code,msr.store_code,'O2O' from merchant_store_relation msr where consum_type->'$.O2O' = true and deleted = 0
+) a
+ON a.mer_code = mac.mer_code and mac.deleted = 0
+where a.id is NOT NULL;
+
+#  福利类型 ONLINE_MALL 配置
+INSERT INTO `mer_account_type_consume_scene_config`( `mer_code`, `mer_account_type_code`, `store_code`, `scene_consume_type`, `create_user`, `create_time`, `update_user`, `update_time`, `deleted`, `version`)
+SELECT  a.mer_code,mac.mer_account_type_code,a.store_code,'ONLINE_MALL','duanhy', now(), NULL, NULL, 0 ,0
+from
+merchant_account_type mac
+LEFT JOIN
+(
+SELECT msr.id, msr.mer_code,msr.store_code,'ONLINE_MALL' from merchant_store_relation msr where consum_type->'$.ONLINE_MALL' = true and deleted = 0
+) a
+ON a.mer_code = mac.mer_code and mac.deleted = 0
+where a.id is NOT NULL;
+
+#  福利类型 SHOP_CONSUMPTION 配置
+INSERT INTO `mer_account_type_consume_scene_config`( `mer_code`, `mer_account_type_code`, `store_code`, `scene_consume_type`, `create_user`, `create_time`, `update_user`, `update_time`, `deleted`, `version`)
+SELECT  a.mer_code,mac.mer_account_type_code,a.store_code,'SHOP_CONSUMPTION','duanhy', now(), NULL, NULL, 0 ,0
+from
+merchant_account_type mac
+LEFT JOIN
+(
+SELECT msr.id, msr.mer_code,msr.store_code,'SHOP_CONSUMPTION' from merchant_store_relation msr where consum_type->'$.SHOP_CONSUMPTION' = true and deleted = 0
+) a
+ON a.mer_code = mac.mer_code and mac.deleted = 0
+where a.id is NOT NULL;
+
+#  福利类型 WHOLESALE 配置
+INSERT INTO `mer_account_type_consume_scene_config`( `mer_code`, `mer_account_type_code`, `store_code`, `scene_consume_type`, `create_user`, `create_time`, `update_user`, `update_time`, `deleted`, `version`)
+SELECT  a.mer_code,mac.mer_account_type_code,a.store_code,'WHOLESALE','duanhy', now(), NULL, NULL, 0 ,0
+from
+merchant_account_type mac
+LEFT JOIN
+(
+SELECT msr.id, msr.mer_code,msr.store_code,'WHOLESALE' from merchant_store_relation msr where consum_type->'$.WHOLESALE' = true and deleted = 0
+) a
+ON a.mer_code = mac.mer_code and mac.deleted = 0
+where a.id is NOT NULL;
+
+# mer_account_type_consume_scene_config 初始化完成 去掉id自增
+ALTER TABLE mer_account_type_consume_scene_config MODIFY COLUMN id BIGINT(20) NOT NULL;
+
+# account_amount_type_group.group_code 序列配置
 INSERT INTO `sequence`(`id`, `sequence_type`, `prefix`, `sequence_no`, `min_sequence`, `max_sequence`, `handler_for_max`, `create_user`, `create_time`, `update_user`, `update_time`, `deleted`, `version`) VALUES (20, 'account_amount_type_group_code', NULL, 1000000000, 1000000000, 9999999999, 'com.welfare.service.sequence.CommonMaxHandler', 'anonymous', '2021-03-08 16:59:23', 'zxadmin', '2021-03-16 10:44:35', 0, 0);

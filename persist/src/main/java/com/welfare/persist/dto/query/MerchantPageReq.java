41fa397ff8c8d65d6a51fa170f9aa2bd3fae7196
duanhy <dhongyuan@foxmail.com>
2021-04-22 11:44:54 +0800

    加日志

--- persist/src/main/java/com/welfare/persist/dto/query/MerchantPageReq.java
+++ persist/src/main/java/com/welfare/persist/dto/query/MerchantPageReq.java
@@ -30,9 +30,6 @@ public class MerchantPageReq {
     @ApiModelProperty("结束时间")
     private Date endTime;
 
-    @ApiModelProperty("余额查询开始时间")
-    private Date balanceStartTime;
-
     @ApiModelProperty("余额查询结束时间")
     private Date balanceEndTime;
 }

592dce014f32ce39bc40627ac5faee82cc2f1eac
duanhy <dhongyuan@foxmail.com>
2021-04-26 13:43:46 +0800

    回充 余额不足

--- busi/src/main/java/com/welfare/service/impl/AccountServiceImpl.java
+++ busi/src/main/java/com/welfare/service/impl/AccountServiceImpl.java
@@ -822,6 +822,10 @@ public class AccountServiceImpl implements AccountService {
             ? surplusQuota.add(surplusQuotaOverpay) : surplusQuota);
         accountSimpleDTO.setSurplusQuotaOverpay(surplusQuotaOverpay);
         accountSimpleDTO.setCredit(account.getCredit());
+        AccountAmountType accountAmountType = accountAmountTypeService.queryOne(accountCode, MerAccountTypeCode.WHOLESALE.code());
+        if (Objects.nonNull(accountAmountType)) {
+            accountSimpleDTO.setWholesaleCredit(accountAmountType.getAccountBalance());
+        }
         return accountSimpleDTO;
     }
 

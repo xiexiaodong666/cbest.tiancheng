5b93323f96f596939a2597921279f92f92e93099
yuxiang.li <kobe663@gmail.com>
2021-04-26 16:31:34 +0800

    批发结算

---
+++ service-settlement/src/main/java/com/welfare/servicesettlement/controller/WholesaleSettleController.java
@@ -0,0 +1,17 @@
+package com.welfare.servicesettlement.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description:
+ *
+ * @author Yuxiang Li
+ * @email yuxiang.li@sjgo365.com
+ * @date 4/26/2021
+ */
+@RestController
+@RequestMapping("/settlement/wholesale/")
+public class WholesaleSettleController {
+
+}

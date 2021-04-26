9749ad702aaf9be11f336e523787487bfb277b0a
duanhy <dhongyuan@foxmail.com>
2021-04-26 17:04:26 +0800

    多门店查询支付渠道

--- busi/src/main/java/com/welfare/service/impl/PaymentChannelConfigServiceImpl.java
+++ busi/src/main/java/com/welfare/service/impl/PaymentChannelConfigServiceImpl.java
@@ -253,7 +253,7 @@ public class PaymentChannelConfigServiceImpl implements PaymentChannelConfigServ
         List<com.welfare.service.dto.PaymentChannelDTO> paymentChannels = paymentChannelService.list(channelReq);
         QueryWrapper<PaymentChannelConfig> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq(PaymentChannelConfig.MER_CODE, req.getMerCode());
-        queryWrapper.eq(PaymentChannelConfig.STORE_CODE, req.getStoreCode());
+        queryWrapper.in(PaymentChannelConfig.STORE_CODE, req.getStoreCodes());
         queryWrapper.eq(PaymentChannelConfig.CONSUME_TYPE, req.getConsumeType());
         List<PaymentChannelConfig> result = new ArrayList<>();
         List<PaymentChannelConfig> channelConfigs = paymentChannelConfigDao.list(queryWrapper);
@@ -261,7 +261,7 @@ public class PaymentChannelConfigServiceImpl implements PaymentChannelConfigServ
             Map<String, List<PaymentChannelConfig>> channelConfigMap = channelConfigs.stream().collect(Collectors.groupingBy(PaymentChannelConfig::getPaymentChannelCode));
             paymentChannels.forEach(dto -> {
                 if (channelConfigMap.containsKey(dto.getPaymentChannelCode())) {
-                    result.add(channelConfigMap.get(dto.getPaymentChannelCode()).get(0));
+                    result.addAll(channelConfigMap.get(dto.getPaymentChannelCode()));
                 }
             });
         }

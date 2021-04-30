ea7aa52190fec95ab3d589ad840795c260ee9e0c
green.gao <cquptgr@gmail.com>
2021-04-30 14:31:45 +0800

    [dev-1.6.0 fix]

--- busi/src/main/java/com/welfare/service/impl/MerchantServiceImpl.java
+++ busi/src/main/java/com/welfare/service/impl/MerchantServiceImpl.java
@@ -215,7 +215,7 @@ public class MerchantServiceImpl implements MerchantService {
             detailDTO.setSettlementMode(method);
         } else {
             detailDTO.setWholesaleEnabled(false);
-            detailDTO.setSettlementMode(null);
+            detailDTO.setSettlementMode("");
         }
         detailDTO.setTags(syncIndustryTag);
         detailDTO.setAddressList(merchant.getAddressList());
@@ -258,7 +258,7 @@ public class MerchantServiceImpl implements MerchantService {
             detailDTO.setSettlementMode(method);
         } else {
             detailDTO.setWholesaleEnabled(false);
-            detailDTO.setSettlementMode(null);
+            detailDTO.setSettlementMode("");
         }
         detailDTO.setTags(industryTags);
         detailDTO.setAddressList(merchant.getAddressList());

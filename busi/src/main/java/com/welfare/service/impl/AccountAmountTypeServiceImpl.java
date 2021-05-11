2d3388f699e0f663c10653a6dfa21e59ece9222d
green.gao <cquptgr@gmail.com>
2021-05-11 14:51:52 +0800

    [chore]

--- busi/src/main/java/com/welfare/service/impl/AccountAmountTypeServiceImpl.java
+++ busi/src/main/java/com/welfare/service/impl/AccountAmountTypeServiceImpl.java
@@ -187,7 +187,7 @@ public class AccountAmountTypeServiceImpl implements AccountAmountTypeService {
                             deposit.getTransNo(),
                             WelfareConstant.TransType.DEPOSIT_INCR));
                 }
-                BizAssert.isTrue(CollectionUtils.isEmpty(errorMsg), ExceptionCode.ILLEGALITY_ARGUMENTS, "员工批发采购账户不可充为负\n" + Joiner.on(",").join(errorMsg));
+                BizAssert.isTrue(CollectionUtils.isEmpty(errorMsg), ExceptionCode.ILLEGALITY_ARGUMENTS, "员工账户不可充为负\n" + Joiner.on(",").join(errorMsg));
                 accountChangeEventRecordDao.getBaseMapper().batchInsert(records);
                 accountAmountTypeDao.saveBatch(newAccountAmountTypes);
                 Map<Long, BigDecimal> amountMap = deposits.stream().collect(Collectors.toMap(Deposit::getAccountCode, Deposit::getAmount));
